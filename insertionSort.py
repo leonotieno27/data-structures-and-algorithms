07fe5820bea1708eac85054ff56b5b4ed32b11a5
leonotieno27 <leonowino99@gmail.com>
2025-01-22 11:39:55 +0300

    implementing insertion sort algorithm

---
+++ insertionSort.py
@@ -0,0 +1,21 @@
+"""
+compares an element with all prevoius elements
+then inserts at desired place. then pick the next element
+"""
+
+def insertion_sort(arr):
+    #traverse from 1 to len of the array
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+arr  = [12, 11, 13, 5, 6]
+print("Original array:", arr)
+
+insertion_sort(arr)
+print("Sorted array:", arr)

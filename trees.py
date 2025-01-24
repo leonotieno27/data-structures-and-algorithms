285e7a51a3c131934812c51111e007df1f37ee7c
leonotieno27 <leonowino99@gmail.com>
2025-01-24 13:30:10 +0300

    implementing binary search tree

--- trees.py
+++ trees.py
@@ -1,3 +1,14 @@
+"""
+used in
+Computer File systems
+BST used in sorted data for quick look ups, insertion and deletions
+    .databases indexing and searching
+    .autocomplete or search suggetsions
+    .symbol tables in compilers
+DOM: manipulation
+
+"""
+#each node can have  multiple children
 class TreeNode:
     def __init__(self, value):
         """"Initialize a tree node with a value and an empty list of children"""
@@ -58,5 +69,47 @@ if __name__ == "__main__":
         print("Node not found.")
 
 
+"""BINARY TREES"""
+#each tree has atmost two children
+class BinaryTreeNode:
+    def __init__(self, value):
+        """"Initialize a binary tree node with a value and left/right children"""
+        self.value = value
+        self.left = None
+        self.right = None
 
+
+    def insert_left(self, value):
+        """"insert a value as a left child"""
+        self.left = BinaryTreeNode(value)
+
+    def insert_right(self, value):
+        """"insert value as right child"""
+        self.right = BinaryTreeNode(value)
+
+    def display(self, level=0):
+        """Display the binary tree structure in a readable format"""
+        print(" " * level +  str(self.value))
+        if self.left:
+            self.left.display(level + 1)
+        if self.right:
+            self.right.display(level + 1)
+
+#building a binary tree
+if __name__ == "__main__":
+    #create root node
+    root = BinaryTreeNode("Root")
+
+    #add left and right children
+    root.insert_left("Left Child")
+    root.insert_right("Right Child")
+
+    #Add grandchildren
+    root.left.insert_left("Left Grandchild")
+    root.left.insert_right("Right Grandchild")
+    root.right.insert_left("Right's child's Left Grandchild")
+
+    #Display the bianry tree
+    print("Binary Tree Structure:")
+    root.display()
 
